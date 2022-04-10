9cfac77369b2bfed93b165f0cba460ae813b66d1
Dimitar <dimiturivanov92@gmail.com>
2022-04-10 23:49:09 +0300

    final commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import moment from "moment";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
 	const [file, setFile] = useState();
@@ -24,14 +25,10 @@ function App() {
 			return obj;
 		});
 
-		setArray(array);
-
-		createProperObj(array);
+		setArray(getDaysInfo(array));
 	};
 
-	console.log(array);
-
-	const createProperObj = (array) => {
+	const getDaysInfo = (array) => {
 		const getDate = (strDate) => {
 			if (strDate.includes("NULL")) return moment().toDate();
 
@@ -40,7 +37,25 @@ function App() {
 			return date.toDate();
 		};
 
-		let twoDArr = [];
+		const calculateDays = (fromA, toA, fromB, toB) => {
+			let distance = 0;
+
+			if (fromA >= fromB && toA <= toB) {
+				distance = toA - fromA;
+			} else if (fromA < fromB && toA > toB) {
+				distance = toB - fromB;
+			} else if (fromA <= fromB && toA <= toB) {
+				distance = toA - fromB;
+			} else if (fromA > fromB && toA >= toB) {
+				distance = toB - fromA;
+			}
+
+			return Math.round(distance / 86400000);
+		};
+
+		if (array.length === 0) return ["No data"];
+
+		let keyValArr = [];
 
 		array.forEach((element) => {
 			const keys = Object.keys(element);
@@ -51,12 +66,12 @@ function App() {
 
 			const splitVals = values[0].split(";");
 
-			twoDArr.push([splitKeys, splitVals]);
+			keyValArr.push([splitKeys, splitVals]);
 		});
 
-		console.log(twoDArr);
+		if (keyValArr.length === 0) return ["No data"];
 
-		const obj = twoDArr.map((arr) => {
+		const objArr = keyValArr.map((arr) => {
 			if (arr[1].length < 4) return;
 			return {
 				EmpID: arr[1][0],
@@ -66,23 +81,56 @@ function App() {
 			};
 		});
 
-		console.log(obj);
+		if (objArr.length === 0) return ["No data"];
 
-		let sameProjects = [];
+		let sameProjectsWork = [];
 
-		for (let i = 0; i < obj.length; i++) {
-			for (let m = i + 1; m < obj.length; m++) {
-				if (obj[i] && obj[m] && obj[i].ProjectID === obj[m].ProjectID) {
-					if (obj[i].EmpID !== obj[m].EmpID) {
-						sameProjects.push([obj[i], obj[m]]);
+		for (let i = 0; i < objArr.length; i++) {
+			for (let m = i + 1; m < objArr.length; m++) {
+				if (
+					objArr[i] &&
+					objArr[m] &&
+					objArr[i].ProjectID === objArr[m].ProjectID
+				) {
+					if (objArr[i].EmpID !== objArr[m].EmpID) {
+						if (
+							objArr[i].DateFrom <= objArr[m].DateTo &&
+							objArr[m].DateFrom <= objArr[i].DateTo
+						) {
+							sameProjectsWork.push([objArr[i], objArr[m]]);
+						}
 					}
 				}
 			}
 		}
 
-		console.log(sameProjects);
+		if (sameProjectsWork.length === 0) return ["No data"];
 
-		let strDate = sameProjects[0][0].DateFrom;
+		let finalData = [];
+
+		sameProjectsWork.forEach((arr) => {
+			finalData.push({
+				EmpID_1: arr[0].EmpID,
+				EmpID_2: arr[1].EmpID,
+				ProjectID: arr[0].ProjectID,
+				Days: calculateDays(
+					arr[0].DateFrom,
+					arr[0].DateTo,
+					arr[1].DateFrom,
+					arr[1].DateTo
+				),
+			});
+		});
+
+		if (finalData.length === 0) return ["No data"];
+
+		let maxValue = finalData.reduce((max, data) =>
+			max.Days > data.Days ? max : data
+		);
+
+		// console.log(maxValue);
+
+		return [maxValue];
 	};
 
 	const handleOnSubmit = (e) => {
@@ -102,7 +150,10 @@ function App() {
 
 	return (
 		<div style={{ textAlign: "center" }}>
-			<h1>REACTJS CSV IMPORT EXAMPLE </h1>
+			<h3 style={{ marginBottom: "2rem" }}>
+				Identify the pair of employees who have worked together on common
+				projects for the longest period of time
+			</h3>
 			<form>
 				<input
 					type={"file"}
@@ -122,20 +173,26 @@ function App() {
 
 			<br />
 
-			<table>
+			<h3>Data:</h3>
+
+			<table style={{ margin: "2rem auto" }}>
 				<thead>
-					<tr key={"header"}>
+					<tr key={uuidv4()}>
 						{headerKeys.map((key) => (
-							<th>{key}</th>
+							<th key={uuidv4()} style={{ paddingLeft: "1rem" }}>
+								{key}
+							</th>
 						))}
 					</tr>
 				</thead>
 
 				<tbody>
 					{array.map((item) => (
-						<tr key={item.id}>
+						<tr key={uuidv4()}>
 							{Object.values(item).map((val) => (
-								<td>{val}</td>
+								<td key={uuidv4()} style={{ paddingLeft: "1rem" }}>
+									{val}
+								</td>
 							))}
 						</tr>
 					))}
